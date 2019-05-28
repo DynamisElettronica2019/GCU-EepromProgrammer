e58da4152911c73f2a189b7c88f445f5086f5ea0
Unknown <marco.mandelli212@gmail.com>
2019-05-28 18:06:35 +0200

    Gui view init

---
+++ src/front_end/gui/GuiController.java
@@ -0,0 +1,16 @@
+package front_end.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class GuiController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/front_end/gui/GuiView.java
@@ -0,0 +1,21 @@
+package front_end.gui;
+
+import back_end.Acceleration;
+import back_end.Gearshift;
+import front_end.View;
+
+public class GuiView extends View {
+
+	@Override
+	public void UpdateGearshift(Gearshift gear) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void UpdateAcceleration(Acceleration accel) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
