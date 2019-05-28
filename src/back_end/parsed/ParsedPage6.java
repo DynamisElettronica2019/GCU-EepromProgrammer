5eca9a1566de9baa8711edc998a8c178b756f277
Unknown <marco.mandelli212@gmail.com>
2019-05-28 18:49:12 +0200

    Parser page 6

--- src/back_end/parsed/ParsedPage6.java
+++ src/back_end/parsed/ParsedPage6.java
@@ -1,16 +1,28 @@
 package back_end.parsed;
 
 public class ParsedPage6 extends Parsed {
+	private final int SPEED_LIMIT_2_3_START = 0;
+	private final int SPEED_LIMIT_2_3_END = 1;
+	private final int SPEED_LIMIT_3_4_START = 2;
+	private final int SPEED_LIMIT_3_4_END = 3;
+	private final int SPEED_LIMIT_4_5_START = 4;
+	private final int SPEED_LIMIT_4_5_END = 5;
+	private final int TPS_START_LIMIT_START = 6;
+	private final int TPS_START_LIMIT_END = 7;
 
 	public ParsedPage6(char messageType) {
 		super(messageType);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void splitString(String string) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * Substrings creation
+		 */
+		parsed.add(string.substring(SPEED_LIMIT_2_3_START, SPEED_LIMIT_2_3_END));
+		parsed.add(string.substring(SPEED_LIMIT_3_4_START, SPEED_LIMIT_3_4_END));
+		parsed.add(string.substring(SPEED_LIMIT_4_5_START, SPEED_LIMIT_4_5_END));
+		parsed.add(string.substring(TPS_START_LIMIT_START, TPS_START_LIMIT_END));
 	}
 
 }
