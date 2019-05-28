825215860ffa647e307e1086b94b0a9afe6070ef
Unknown <marco.mandelli212@gmail.com>
2019-05-28 21:35:13 +0200

    Configuration file parser page 3

--- src/back_end/parsed/ParsedPage3.java
+++ src/back_end/parsed/ParsedPage3.java
@@ -1,5 +1,7 @@
 package back_end.parsed;
 
+import configuration.Channels;
+
 public class ParsedPage3 extends Parsed {
 	private final int DELAY_START = 0;
 	private final int DELAY_END = 1;
@@ -26,12 +28,26 @@ public class ParsedPage3 extends Parsed {
 		 * Substrings creation
 		 */
 		parsed.add(string.substring(DELAY_START, DELAY_END));
+		parsedName.add(Channels.DELAY_NAME);
+		parsedMap.put(Channels.DELAY_NAME, 0);
 		parsed.add(string.substring(UP_REBOUND_START, UP_REBOUND_END));
+		parsedName.add(Channels.UP_REBOUND_NAME);
+		parsedMap.put(Channels.UP_REBOUND_NAME, 0);
 		parsed.add(string.substring(UP_BRAKE_START, UP_BRAKE_END));
+		parsedName.add(Channels.UP_BRAKE_NAME);
+		parsedMap.put(Channels.UP_BRAKE_NAME, 0);
 		parsed.add(string.substring(NT_CLUTCH_DELAY_START, NT_CLUTCH_DELAY_END));
+		parsedName.add(Channels.NT_CLUTCH_DELAY_NAME);
+		parsedMap.put(Channels.NT_CLUTCH_DELAY_NAME, 0);
 		parsed.add(string.substring(DOWN_TIME_CHECK_START, DOWN_TIME_CHECK_END));
+		parsedName.add(Channels.DOWN_TIME_CHECK_NAME);
+		parsedMap.put(Channels.DOWN_TIME_CHECK_NAME, 0);
 		parsed.add(string.substring(UP_TIME_CHECK_START, UP_TIME_CHECK_END));
+		parsedName.add(Channels.UP_TIME_CHECK_NAME);
+		parsedMap.put(Channels.UP_TIME_CHECK_NAME, 0);
 		parsed.add(string.substring(MAX_TRIES_START, MAX_TRIES_END));
+		parsedName.add(Channels.MAX_TRIES_NAME);
+		parsedMap.put(Channels.MAX_TRIES_NAME, 0);
 	}
 
 }
