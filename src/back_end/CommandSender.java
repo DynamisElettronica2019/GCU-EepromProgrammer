a3f99d030e279c40976ee56e5267f0eb943b59f7
Unknown <marco.mandelli212@gmail.com>
2019-05-28 21:57:56 +0200

    CommandSender constructor and send function

--- src/back_end/CommandSender.java
+++ src/back_end/CommandSender.java
@@ -1,5 +1,28 @@
 package back_end;
 
-public class CommandSender {
+import java.util.ArrayList;
 
+import front_end.View;
+
+public class CommandSender {
+	private Receiver receiver;
+	private Data data;
+
+	/*
+	 * Create a new command sender associated with a specific 'receiver' and a specific 'data'. Set the command sender as command sender of all views
+	 */
+	public CommandSender(Receiver receiver, Data data, ArrayList<View> myViews) {
+		this.receiver=receiver;
+		this.data=data;
+		for(View v: myViews){
+			v.setCommandSender(this);
+		}
+	}
+	
+	/*
+	 * Call receiver send function
+	 */
+	public void sendCommand(String comm) {
+		receiver.send(comm);
+	}
 }
