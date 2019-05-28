302b81856b69af3bb824b81f034c4df8026306dc
Unknown <marco.mandelli212@gmail.com>
2019-05-28 21:55:06 +0200

    Added view in main + comments

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,12 +6,20 @@ import back_end.Data;
 import back_end.Parser;
 import back_end.Receiver;
 import front_end.View;
+import front_end.gui.GuiView;
 
 public class Main {
 
 	public static void main(String[] args) {
+		/*
+		 * Create view
+		 */
 		ArrayList<View> myViews = new ArrayList<View>();
+		myViews.add(new GuiView());
 		
+		/*
+		 * Create data, parser and start receiver
+		 */
 		Data data = new Data(myViews);
 		Parser parser = new Parser(data);
 		
