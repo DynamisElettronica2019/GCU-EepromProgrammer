7ff7bf306b15724382e3c1ce3341e27d1afb031c
Unknown <marco.mandelli212@gmail.com>
2019-05-29 08:34:31 +0200

    Command sender send calls

--- src/back_end/CommandSender.java
+++ src/back_end/CommandSender.java
@@ -3,6 +3,12 @@ package back_end;
 import java.util.ArrayList;
 
 import back_end.sender.SenderPage1;
+import back_end.sender.SenderPage2;
+import back_end.sender.SenderPage3;
+import back_end.sender.SenderPage4;
+import back_end.sender.SenderPage5;
+import back_end.sender.SenderPage6;
+import back_end.sender.SenderPage7;
 import configuration.Channels;
 import front_end.View;
 
@@ -34,6 +40,39 @@ public class CommandSender {
 	 */
 	public void sendNewDataPage1() {
 		SenderPage1 sender = new SenderPage1(receiver);
+		sender.Send(data.getGearshift());
+	}
+	
+	public void sendNewDataPage2() {
+		SenderPage2 sender = new SenderPage2(receiver);
+		sender.Send(data.getGearshift());
+	}
+	
+	public void sendNewDataPage3() {
+		SenderPage3 sender = new SenderPage3(receiver);
+		sender.Send(data.getGearshift());
+	}
+	
+	public void sendNewDataPage4() {
+		SenderPage4 sender = new SenderPage4(receiver);
+		sender.Send(data.getGearshift());
+	}
+	
+	/*
+	 * Send gearshift page
+	 */
+	public void sendNewDataPage5() {
+		SenderPage5 sender = new SenderPage5(receiver);
+		sender.Send(data.getGearshift());
+	}
+	
+	public void sendNewDataPage6() {
+		SenderPage6 sender = new SenderPage6(receiver);
+		sender.Send(data.getGearshift());
+	}
+	
+	public void sendNewDataPage7() {
+		SenderPage7 sender = new SenderPage7(receiver);
 		sender.Send(data.getGearshift());
 	}
 }
