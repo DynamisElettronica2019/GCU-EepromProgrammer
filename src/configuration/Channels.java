7ebe753629049587c78366b2631328ba2812aa68
Unknown <marco.mandelli212@gmail.com>
2019-06-09 13:51:31 +0200

    Configuration file update

--- src/configuration/Channels.java
+++ src/configuration/Channels.java
@@ -23,6 +23,10 @@ public class Channels {
 	public static final char PAGE_5_ID = 5;
 	public static final char PAGE_6_ID = 6;
 	public static final char PAGE_7_ID = 7;
+	public static final char PAGE_8_ID = 8;
+	public static final char PAGE_9_ID = 9;
+	public static final char PAGE_10_ID = 10;
+	public static final char PAGE_11_ID = 11;
 	
 	/*
 	 * Send costants
@@ -98,10 +102,43 @@ public class Channels {
 	public static final String SPEED_LIMIT_3_4_NAME = "SPEED_LIMIT_3_4";
 	public static final String SPEED_LIMIT_4_5_NAME = "SPEED_LIMIT_4_5";
 	public static final String TPS_START_LIMIT_NAME = "TPS_START_LIMIT";
+	public static final String END_GEAR_AC_NAME = "END_GEAR_AC";
 	
 	/*
 	 * Page 7 channel names
 	 */
+	
+	/*
+	 * Page 8 channel names
+	 */
+	public static final String RAMP_START_AU_NAME = "RAMP_START_AU";
+	public static final String RAMP_END_AU_NAME = "RAMP_END_AU";
+	public static final String RAMP_TIME_AU_NAME = "RAMP_TIME_AU";
+	public static final String RPM_LIMIT_AU_1_2_NAME = "RPM_LIMIT_AU_1_2";
+	public static final String RPM_LIMIT_AU_2_3_NAME = "RPM_LIMIT_AU_2_3";
+	public static final String RPM_LIMIT_AU_3_4_NAME = "RPM_LIMIT_AU_3_4";
+	public static final String RPM_LIMIT_AU_4_5_NAME = "RPM_LIMIT_AU_4_5";
+	public static final String SPEED_LIMIT_AU_1_2_NAME = "SPEED_LIMIT_AU_1_2";
+	
+	/*
+	 * Page 9 channel names
+	 */
+	public static final String SPEED_LIMIT_AU_2_3_NAME = "SPEED_LIMIT_AU_2_3";
+	public static final String SPEED_LIMIT_AU_3_4_NAME = "SPEED_LIMIT_AU_3_4";
+	public static final String SPEED_LIMIT_AU_4_5_NAME = "SPEED_LIMIT_AU_4_5";
+	public static final String TPS_START_AU_LIMIT_NAME = "TPS_START_LIMIT_AU";
+	public static final String END_GEAR_AU_NAME = "END_GEAR_AU";
+	
+	/*
+	 * Page 10 channel names
+	 */
+	
+	/*
+	 * Page 11 channel names
+	 */
+	public static final String RPM_TRESHOLD_NAME = "RPM_TRESHOLD";
+	public static final String DELTA_RPM_TRESHOLD_NAME = "DELTA_RPM_TRESHOLD";
+	public static final String GEAR_TRESHOLD_NAME = "GEAR_TRESHOLD";
 	
 	/*
 	 * Page indexes
@@ -112,8 +149,12 @@ public class Channels {
 	public static final int PAGE3_SIZE = 7;
 	public static final int PAGE4_SIZE = 0;
 	public static final int PAGE5_SIZE = 8;
-	public static final int PAGE6_SIZE = 4;
+	public static final int PAGE6_SIZE = 5;
 	public static final int PAGE7_SIZE = 0;
+	public static final int PAGE8_SIZE = 8;
+	public static final int PAGE9_SIZE = 5;
+	public static final int PAGE10_SIZE = 0;
+	public static final int PAGE11_SIZE = 3;
 	
 	public static final int PAGE0_INDEX_START = 0;
 	public static final int PAGE0_INDEX_END = PAGE0_INDEX_START + PAGE0_SIZE - 1;
@@ -131,6 +172,14 @@ public class Channels {
 	public static final int PAGE6_INDEX_END = PAGE6_INDEX_START + PAGE6_SIZE - 1;
 	public static final int PAGE7_INDEX_START = PAGE6_INDEX_END + 1;
 	public static final int PAGE7_INDEX_END = PAGE7_INDEX_START + PAGE7_SIZE - 1;
+	public static final int PAGE8_INDEX_START = PAGE7_INDEX_END + 1;
+	public static final int PAGE8_INDEX_END = PAGE8_INDEX_START + PAGE8_SIZE - 1;
+	public static final int PAGE9_INDEX_START = PAGE8_INDEX_END + 1;
+	public static final int PAGE9_INDEX_END = PAGE9_INDEX_START + PAGE9_SIZE - 1;
+	public static final int PAGE10_INDEX_START = PAGE9_INDEX_END + 1;
+	public static final int PAGE10_INDEX_END = PAGE10_INDEX_START + PAGE10_SIZE - 1;
+	public static final int PAGE11_INDEX_START = PAGE10_INDEX_END + 1;
+	public static final int PAGE11_INDEX_END = PAGE11_INDEX_START + PAGE11_SIZE - 1;
 	
 	/*
 	 * Type sizes
@@ -139,6 +188,8 @@ public class Channels {
 	public static final int DEBUG_SIZE = PAGE0_SIZE;
 	public static final int GEARSHIFT_SIZE = PAGE1_SIZE + PAGE2_SIZE + PAGE3_SIZE + PAGE4_SIZE;
 	public static final int ACCELERATION_SIZE = PAGE5_SIZE + PAGE6_SIZE + PAGE7_SIZE;
+	public static final int AUTOCROSS_SIZE = PAGE8_SIZE + PAGE9_SIZE + PAGE10_SIZE;
+	public static final int ANTISTALL_SIZE = PAGE11_SIZE;
 	
 	/*
 	 * Page 0 indexes
@@ -245,10 +296,60 @@ public class Channels {
 	public static final int SPEED_LIMIT_4_5_END = 5;
 	public static final int TPS_START_LIMIT_START = 6;
 	public static final int TPS_START_LIMIT_END = 7;
+	public static final int END_GEAR_AC_START = 8;
+	public static final int END_GEAR_AC_END = 9;
 	
 	/*
 	 * Page 7 indexes
 	 */
+	
+	/*
+	 * Page 8 indexes
+	 */
+	public static final int RAMP_START_AU_START = 0;
+	public static final int RAMP_START_AU_END = 1;
+	public static final int RAMP_END_AU_START = 2;
+	public static final int RAMP_END_AU_END = 3;
+	public static final int RAMP_TIME_AU_START = 4;
+	public static final int RAMP_TIME_AU_END = 5;
+	public static final int RPM_LIMIT_AU_1_2_START = 6;
+	public static final int RPM_LIMIT_AU_1_2_END = 7;
+	public static final int RPM_LIMIT_AU_2_3_START = 8;
+	public static final int RPM_LIMIT_AU_2_3_END = 9;
+	public static final int RPM_LIMIT_AU_3_4_START = 10;
+	public static final int RPM_LIMIT_AU_3_4_END = 11;
+	public static final int RPM_LIMIT_AU_4_5_START = 12;
+	public static final int RPM_LIMIT_AU_4_5_END = 13;
+	public static final int SPEED_LIMIT_AU_1_2_START = 14;
+	public static final int SPEED_LIMIT_AU_1_2_END = 15;
+	
+	/*
+	 * Page 9 indexes
+	 */
+	public static final int SPEED_LIMIT_AU_2_3_START = 0;
+	public static final int SPEED_LIMIT_AU_2_3_END = 1;
+	public static final int SPEED_LIMIT_AU_3_4_START = 2;
+	public static final int SPEED_LIMIT_AU_3_4_END = 3;
+	public static final int SPEED_LIMIT_AU_4_5_START = 4;
+	public static final int SPEED_LIMIT_AU_4_5_END = 5;
+	public static final int TPS_START_LIMIT_AU_START = 6;
+	public static final int TPS_START_LIMIT_AU_END = 7;
+	public static final int END_GEAR_AU_START = 8;
+	public static final int END_GEAR_AU_END = 9;
+	
+	/*
+	 * Page 10 indexes
+	 */
+	
+	/*
+	 * Page 11 indexes
+	 */
+	public static final int RPM_TRESHOLD_START = 0;
+	public static final int RPM_TRESHOLD_END = 1;
+	public static final int DELTA_RPM_TRESHOLD_START = 2;
+	public static final int DELTA_RPM_TRESHOLD_END = 3;
+	public static final int GEAR_TRESHOLD_START = 4;
+	public static final int GEAR_TRESHOLD_END = 5;
 	
 	
 	public Channels() {
@@ -299,6 +400,26 @@ public class Channels {
 		channelNames.add(SPEED_LIMIT_3_4_NAME);
 		channelNames.add(SPEED_LIMIT_4_5_NAME);
 		channelNames.add(TPS_START_LIMIT_NAME);
+		channelNames.add(END_GEAR_AC_NAME);
+		
+		channelNames.add(RAMP_START_AU_NAME);
+		channelNames.add(RAMP_END_AU_NAME);
+		channelNames.add(RAMP_TIME_AU_NAME);
+		channelNames.add(RPM_LIMIT_AU_1_2_NAME);
+		channelNames.add(RPM_LIMIT_AU_2_3_NAME);
+		channelNames.add(RPM_LIMIT_AU_3_4_NAME);
+		channelNames.add(RPM_LIMIT_AU_4_5_NAME);
+		channelNames.add(SPEED_LIMIT_AU_1_2_NAME);
+		
+		channelNames.add(SPEED_LIMIT_AU_2_3_NAME);
+		channelNames.add(SPEED_LIMIT_AU_3_4_NAME);
+		channelNames.add(SPEED_LIMIT_AU_4_5_NAME);
+		channelNames.add(TPS_START_AU_LIMIT_NAME);
+		channelNames.add(END_GEAR_AU_NAME);
+		
+		channelNames.add(RPM_TRESHOLD_NAME);
+		channelNames.add(DELTA_RPM_TRESHOLD_NAME);
+		channelNames.add(GEAR_TRESHOLD_NAME);
 	}
 
 	public static ArrayList<String> getChannelNames() {
